e63a3b1a30422d522bbd1a7c0205fbcba030a769
Josh Fuoco <josh.fuoco@jacobs.com>
2022-08-03 13:42:17 -0400

    fix(cssgrid): v0.4.14 fix for transitions

--- src/components/CSSGrid.jsx
+++ src/components/CSSGrid.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { TransitionGroup } from 'react-transition-group';
+import TransitionGroup from 'react-transition-group-plus';
 import shallowEqual from 'shallowequal';
 import omit from 'lodash.omit';
 import { commonPropTypes, commonDefaultProps } from '../utils/commonProps';
