6ca2281b123086c539c737d65dc59fe41e8b5c65
Josh Fuoco <josh.fuoco@jacobs.com>
2022-08-15 14:15:35 -0400

    clearing branch

--- src/enter-exit-styles/fromLeftToRight.js
+++
@@ -1,8 +0,0 @@
-export const enter = () => ({ translateX: -500, opacity: 0 });
-
-export const exit = (itemProps, gridProps, gridState) => ({
-  translateX: gridState.gridWidth + 500,
-  opacity: 0
-});
-
-export const entered = () => ({ opacity: 1 });
--- src/enter-exit-styles/fromTop.js
+++
@@ -1,6 +0,0 @@
-const enterExit = () => ({ translateY: 0, opacity: 0 });
-
-export const enter = enterExit;
-export const exit = enterExit;
-
-export const entered = () => ({ opacity: 1 });
